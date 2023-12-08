ee7c94042e30e03b0698ecaeac62f4bb3d649481
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-12-08 16:01:58 +0800

    Create axioma-addons.php

---
+++ axioma-addons.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:       Axioma Add-Ons Product Percentage
+ * Plugin URI:        https://yourpropfirm.com/
+ * Description:       Add-Ons Product Percentage and Calculation Total Order on Checkout Woocommerce
+ * Version:           1.0.1
+ * Author:            Ardi
+ * Author URI:        https://yourpropfirm.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       axioma-addons
+ * Domain Path:       /languages
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
